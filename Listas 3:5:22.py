47c47f5b1802b90de02b1253273ec3e5e031ac10
DianaKarenR <104459729+DianaKarenR@users.noreply.github.com>
2022-05-03 13:48:50 -0500

    Probanto
    Probando

---
+++ Listas 3:5:22.py
@@ -0,0 +1,21 @@
+frutas=["mango", "limon"]
+
+print(frutas)
+print(frutas[1])
+
+print(len(frutas))
+
+frutas.append("fresa")
+print(frutas)
+
+frutas.append("kiwi")
+print(frutas)
+
+frutas.sort
+print(frutas)
+
+porcomprar=input("ingrese una fruta->")
+
+frutas.append(porcomprar)
+
+print(frutas)
